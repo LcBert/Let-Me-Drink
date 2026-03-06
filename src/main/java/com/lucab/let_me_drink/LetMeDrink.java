b5bc1b9bd3cfc1b501786628bad81a43dc47fc55
Luca Bertaggia <luca.bertaggia82@gmail.com>
2026-03-06 14:59:51 +0100

    Code clean

---
+++ src/main/java/com/lucab/let_me_drink/LetMeDrink.java
@@ -0,0 +1,30 @@
+package com.lucab.let_me_drink;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(LetMeDrink.MODID)
+public class LetMeDrink {
+    public static final String MODID = "let_me_drink";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+
+    public LetMeDrink(IEventBus modEventBus, ModContainer modContainer) {
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+    }
+}
